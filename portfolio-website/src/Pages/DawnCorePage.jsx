0a08afb855b045f7a04329b729e847b6a9c353cf
ScarletFangs <sirkao322@gmail.com>
2025-10-13 12:51:05 -0700

    Updated Resume + Linked Dawncore Itch.io

--- portfolio-website/src/Pages/DawnCorePage.jsx
+++ portfolio-website/src/Pages/DawnCorePage.jsx
@@ -41,46 +41,45 @@ const DawnCorePage = () => {
           <img src={DCPhoto2}/>
       </div>
 
-      <Link to="https://scarletfangs.itch.io/headhunted" target="_blank" className="HeadHunted-LinkBtn" onClick={(e) => e.preventDefault()}>
-          <p>Coming Soon</p>
+      <Link to="https://bronpro.itch.io/dawncore" target="_blank" className="HeadHunted-LinkBtn">
+          <p>To Itch.io</p>
           <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#999999" viewBox="0 0 256 256">
               <path d="M221.66,133.66l-72,72a8,8,0,0,1-11.32-11.32L196.69,136H40a8,8,0,0,1,0-16H196.69L138.34,61.66a8,8,0,0,1,11.32-11.32l72,72A8,8,0,0,1,221.66,133.66Z"></path>
           </svg>
       </Link>
 
       <div className="HeadHunted-MyContributions-Container">
-          <h2>My Contributions</h2>
-          <ul>
-              <li>Developed a movement system that smoothly transitions between modes while maintaining player momentum using linear algebra</li>
-              <li>Collaborated with game designers to ensure fluidity between movement mechanics and maintain satisfying feeling</li>
-              <li>Designed and implemented the pistol and saber weapons in game drawing inspiration from Left for Dead 2</li>
-              <li>Collaborated with UI/UX members to implement user interfaces that enhances gameplay</li>
-              <li>Collaborated with 3D model artist and animators to ensure proper assets are being created for the player</li>
-          </ul>
-      </div>
-
-      <div className="HHVideo" style={{paddingTop: "16px"}}>
-          <video controls width="100%" muted>
-              <source src={DCVideo} type="video/mp4"/>
-              {/* Fallback if video doesnt load */}
-              Your browser does not support video tag. 
-          </video>
+        <h2>My Contributions</h2>
+        <ul>
+            <li>Developed a movement system that smoothly transitions between modes while maintaining player momentum using linear algebra</li>
+            <li>Collaborated with game designers to ensure fluidity between movement mechanics and maintain satisfying feeling</li>
+            <li>Designed and implemented the pistol and saber weapons in game drawing inspiration from Left for Dead 2</li>
+            <li>Collaborated with UI/UX members to implement user interfaces that enhances gameplay</li>
+            <li>Collaborated with 3D model artist and animators to ensure proper assets are being created for the player</li>
+        </ul>
+        <div className="HHVideo" style={{paddingTop: "16px"}}>
+            <video controls width="100%" muted>
+                <source src={DCVideo} type="video/mp4"/>
+                {/* Fallback if video doesnt load */}
+                Your browser does not support video tag. 
+            </video>
+        </div>
       </div>
 
       <div className="HeadHunted-MyContributions-Container">
-          <h2>Weapon Prototype</h2>
+        <h2>Weapon Prototype</h2>
+        <div className="HHVideo" style={{paddingTop: "16px"}}>
+            <video controls width="100%" muted>
+                <source src={DCVideo2} type="video/mp4"/>
+                {/* Fallback if video doesnt load */}
+                Your browser does not support video tag. 
+            </video>
+        </div>
       </div>
 
-      <div className="HHVideo" style={{paddingTop: "16px"}}>
-          <video controls width="100%" muted>
-              <source src={DCVideo2} type="video/mp4"/>
-              {/* Fallback if video doesnt load */}
-              Your browser does not support video tag. 
-          </video>
-      </div>
 
-      <Link to="https://scarletfangs.itch.io/headhunted" target="_blank" className="HeadHunted-LinkBtn" onClick={(e) => e.preventDefault()}>
-          <p>Coming Soon</p>
+      <Link to="https://bronpro.itch.io/dawncore" target="_blank" className="HeadHunted-LinkBtn">
+          <p>To Itch.io</p>
           <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#999999" viewBox="0 0 256 256">
               <path d="M221.66,133.66l-72,72a8,8,0,0,1-11.32-11.32L196.69,136H40a8,8,0,0,1,0-16H196.69L138.34,61.66a8,8,0,0,1,11.32-11.32l72,72A8,8,0,0,1,221.66,133.66Z"></path>
           </svg>
